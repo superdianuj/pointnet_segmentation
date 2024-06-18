d0888f5651ec10bac9c05803a46af3d3f089605f
antidianuj <dianujkotov15@gmail.com>
2024-06-19 06:55:01 +0900

    v1.1

---
+++ point_net/main_segment.py
@@ -0,0 +1,333 @@
+import os
+import re
+from glob import glob
+import time
+import random
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchmetrics
+from torchmetrics.classification import MulticlassMatthewsCorrCoef
+import open3d as o3
+# from open3d import JVisualizer # For Colab Visualization
+# from open3d.web_visualizer import draw # for non Colab
+
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# dataset
+ROOT = r'/home/kann/codework/test_pointnet_seg/3D/point_net/Stanford3dDataset_v1.2_Reduced_Partitioned_Aligned_Version'
+
+# feature selection hyperparameters
+NUM_TRAIN_POINTS = 4096 # train/valid points
+NUM_TEST_POINTS = 15000
+
+BATCH_SIZE = 16
+
+CATEGORIES = {
+    'ceiling'  : 0,
+    'floor'    : 1,
+    'wall'     : 2,
+    'beam'     : 3,
+    'column'   : 4,
+    'window'   : 5,
+    'door'     : 6,
+    'table'    : 7,
+    'chair'    : 8,
+    'sofa'     : 9,
+    'bookcase' : 10,
+    'board'    : 11,
+    'stairs'   : 12,
+    'clutter'  : 13
+}
+
+# unique color map generated via
+# https://mokole.com/palette.html
+COLOR_MAP = {
+    0  : (47, 79, 79),    # ceiling - darkslategray
+    1  : (139, 69, 19),   # floor - saddlebrown
+    2  : (34, 139, 34),   # wall - forestgreen
+    3  : (75, 0, 130),    # beam - indigo
+    4  : (255, 0, 0),     # column - red
+    5  : (255, 255, 0),   # window - yellow
+    6  : (0, 255, 0),     # door - lime
+    7  : (0, 255, 255),   # table - aqua
+    8  : (0, 0, 255),     # chair - blue
+    9  : (255, 0, 255),   # sofa - fuchsia
+    10 : (238, 232, 170), # bookcase - palegoldenrod
+    11 : (100, 149, 237), # board - cornflower
+    12 : (255, 105, 180), # stairs - hotpink
+    13 : (0, 0, 0)        # clutter - black
+}
+
+v_map_colors = np.vectorize(lambda x : COLOR_MAP[x])
+
+NUM_CLASSES = len(CATEGORIES)
+
+
+from torch.utils.data import DataLoader
+from s3dis_dataset import S3DIS
+
+# get datasets
+s3dis_train = S3DIS(ROOT, area_nums='1-4', npoints=NUM_TRAIN_POINTS, r_prob=0.25)
+s3dis_valid = S3DIS(ROOT, area_nums='5', npoints=NUM_TRAIN_POINTS, r_prob=0.)
+s3dis_test = S3DIS(ROOT, area_nums='6', split='test', npoints=NUM_TEST_POINTS)
+
+# get dataloaders
+train_dataloader = DataLoader(s3dis_train, batch_size=BATCH_SIZE, shuffle=True)
+valid_dataloader = DataLoader(s3dis_valid, batch_size=BATCH_SIZE, shuffle=True)
+test_dataloader = DataLoader(s3dis_test, batch_size=BATCH_SIZE, shuffle=False)
+
+points, targets = s3dis_train[100]
+
+points,targets=next(iter(train_dataloader))
+
+print("Points Shape: ",points.shape)
+print("Targets Shape: ",targets.shape)
+
+
+# draw(pcd)
+from point_net import PointNetSegHead
+
+points, targets = next(iter(train_dataloader))
+
+seg_model = PointNetSegHead(num_points=NUM_TRAIN_POINTS, m=NUM_CLASSES)
+out, _, _ = seg_model(points.transpose(2, 1))
+print(f'Seg shape: {out.shape}')
+
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+import torch.optim as optim
+from point_net_loss import PointNetSegLoss
+
+EPOCHS = 0
+LR = 0.0001
+
+# use inverse class weighting
+# alpha = 1 / class_bins
+# alpha = (alpha/alpha.max())
+
+# manually set alpha weights
+alpha = np.ones(len(CATEGORIES))
+alpha[0:3] *= 0.25 # balance background classes
+alpha[-1] *= 0.75  # balance clutter class
+
+gamma = 1
+
+optimizer = optim.Adam(seg_model.parameters(), lr=LR)
+scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=1e-6, max_lr=1e-3, 
+                                              step_size_up=1000, cycle_momentum=False)
+criterion = PointNetSegLoss(alpha=alpha, gamma=gamma, dice=True).to(DEVICE)
+
+seg_model = seg_model.to(DEVICE)
+
+mcc_metric = MulticlassMatthewsCorrCoef(num_classes=NUM_CLASSES).to(DEVICE)
+
+def compute_iou(targets, predictions):
+
+    targets = targets.reshape(-1)
+    predictions = predictions.reshape(-1)
+
+    intersection = torch.sum(predictions == targets) # true positives
+    union = len(predictions) + len(targets) - intersection
+
+    return intersection / union
+
+# store best validation iou
+best_iou = 0.6
+best_mcc = 0.6
+
+# lists to store metrics
+train_loss = []
+train_accuracy = []
+train_mcc = []
+train_iou = []
+valid_loss = []
+valid_accuracy = []
+valid_mcc = []
+valid_iou = []
+
+# stuff for training
+num_train_batch = int(np.ceil(len(s3dis_train)/BATCH_SIZE))
+num_valid_batch = int(np.ceil(len(s3dis_valid)/BATCH_SIZE))
+
+for epoch in range(1, EPOCHS + 1):
+    # place model in training mode
+    seg_model = seg_model.train()
+    _train_loss = []
+    _train_accuracy = []
+    _train_mcc = []
+    _train_iou = []
+    for i, (points, targets) in enumerate(train_dataloader, 0):
+
+        points = points.transpose(2, 1).to(DEVICE)
+        targets = targets.squeeze().to(DEVICE)
+
+        # zero gradients
+        optimizer.zero_grad()
+
+        # get predicted class logits
+        preds, _, _ = seg_model(points)
+
+        # get class predictions
+        pred_choice = torch.softmax(preds, dim=2).argmax(dim=2)
+
+        # get loss and perform backprop
+        loss = criterion(preds, targets, pred_choice)
+        loss.backward()
+        optimizer.step()
+        scheduler.step() # update learning rate
+
+        # get metrics
+        correct = pred_choice.eq(targets.data).cpu().sum()
+        accuracy = correct/float(BATCH_SIZE*NUM_TRAIN_POINTS)
+        mcc = mcc_metric(preds.transpose(2, 1), targets)
+        iou = compute_iou(targets, pred_choice)
+
+        # update epoch loss and accuracy
+        _train_loss.append(loss.item())
+        _train_accuracy.append(accuracy)
+        _train_mcc.append(mcc.item())
+        _train_iou.append(iou.item())
+
+        if i % 1 == 0:
+            print(f'\t [{epoch}: {i}/{num_train_batch}] ' \
+                  + f'train loss: {loss.item():.4f} ' \
+                  + f'accuracy: {accuracy:.4f} ' \
+                  + f'mcc: {mcc:.4f} ' \
+                  + f'iou: {iou:.4f}')
+
+    train_loss.append(np.mean(_train_loss))
+    train_accuracy.append(np.mean(_train_accuracy))
+    train_mcc.append(np.mean(_train_mcc))
+    train_iou.append(np.mean(_train_iou))
+
+    print(f'Epoch: {epoch} - Train Loss: {train_loss[-1]:.4f} ' \
+          + f'- Train Accuracy: {train_accuracy[-1]:.4f} ' \
+          + f'- Train MCC: {train_mcc[-1]:.4f} ' \
+          + f'- Train IOU: {train_iou[-1]:.4f}')
+
+    # pause to cool down
+    time.sleep(4)
+
+    # get test results after each epoch
+    with torch.no_grad():
+
+        # place model in evaluation mode
+        seg_model = seg_model.eval()
+
+        _valid_loss = []
+        _valid_accuracy = []
+        _valid_mcc = []
+        _valid_iou = []
+        for i, (points, targets) in enumerate(valid_dataloader, 0):
+
+            points = points.transpose(2, 1).to(DEVICE)
+            targets = targets.squeeze().to(DEVICE)
+
+            preds, _, A = seg_model(points)
+            pred_choice = torch.softmax(preds, dim=2).argmax(dim=2)
+
+            loss = criterion(preds, targets, pred_choice)
+
+            # get metrics
+            correct = pred_choice.eq(targets.data).cpu().sum()
+            accuracy = correct/float(BATCH_SIZE*NUM_TRAIN_POINTS)
+            mcc = mcc_metric(preds.transpose(2, 1), targets)
+            iou = compute_iou(targets, pred_choice)
+
+            # update epoch loss and accuracy
+            _valid_loss.append(loss.item())
+            _valid_accuracy.append(accuracy)
+            _valid_mcc.append(mcc.item())
+            _valid_iou.append(iou.item())
+
+            if i % 1 == 0:
+                print(f'\t [{epoch}: {i}/{num_valid_batch}] ' \
+                  + f'valid loss: {loss.item():.4f} ' \
+                  + f'accuracy: {accuracy:.4f} '
+                  + f'mcc: {mcc:.4f} ' \
+                  + f'iou: {iou:.4f}')
+
+        valid_loss.append(np.mean(_valid_loss))
+        valid_accuracy.append(np.mean(_valid_accuracy))
+        valid_mcc.append(np.mean(_valid_mcc))
+        valid_iou.append(np.mean(_valid_iou))
+        print(f'Epoch: {epoch} - Valid Loss: {valid_loss[-1]:.4f} ' \
+              + f'- Valid Accuracy: {valid_accuracy[-1]:.4f} ' \
+              + f'- Valid MCC: {valid_mcc[-1]:.4f} ' \
+              + f'- Valid IOU: {valid_iou[-1]:.4f}')
+
+
+        # pause to cool down
+        time.sleep(4)
+
+    # save best models
+    if valid_iou[-1] >= best_iou:
+        best_iou = valid_iou[-1]
+        torch.save(seg_model.state_dict(), f'trained_models/seg_focal_dice_iou_rot_clr/seg_model_{epoch}.pth')
+
+
+torch.save(seg_model.state_dict(), f'lesbian.pth')
+model=PointNetSegHead(num_points=NUM_TEST_POINTS,m=NUM_CLASSES).to(DEVICE)
+model.load_state_dict(torch.load('lesbian.pth'))
+model.eval()
+#model=seg_model
+
+torch.cuda.empty_cache() # release GPU memory
+points, targets = next(iter(test_dataloader))
+# points,targets=points[0:1],targets[0:1]
+
+# place on device
+points = points.to(DEVICE)
+targets = targets.to(DEVICE)
+
+print(points.shape)
+print(targets.shape)
+
+
+# Normalize each partitioned Point Cloud to (0, 1)
+norm_points = points.clone()
+norm_points = norm_points - norm_points.min(axis=1)[0].unsqueeze(1)
+norm_points /= norm_points.max(axis=1)[0].unsqueeze(1)
+print(norm_points.shape)
+
+
+
+
+with torch.no_grad():
+
+    # prepare data
+    norm_points = norm_points.transpose(2, 1)
+    targets = targets.squeeze()
+
+    # run inference
+    preds, _, _ = model(norm_points)
+
+    # get metrics
+    pred_choice = torch.softmax(preds, dim=2).argmax(dim=2)
+
+    loss = criterion(preds, targets, pred_choice)
+    correct = pred_choice.eq(targets.data).cpu().sum()
+    accuracy = correct/float(points.shape[0]*NUM_TEST_POINTS)
+    mcc = mcc_metric(preds.transpose(2, 1), targets)
+    iou = compute_iou(targets, pred_choice)
+
+print(f'Loss: {loss:.4f} - Accuracy: {accuracy:.4f} - MCC: {mcc:.4f} - IOU: {iou:.4f}')
+
+
+pcd = o3.geometry.PointCloud()
+pcd.points = o3.utility.Vector3dVector(points.permute(2, 0, 1).reshape(3, -1).to('cpu').T)
+pcd.colors = o3.utility.Vector3dVector(np.vstack(v_map_colors(targets.reshape(-1).to('cpu'))).T/255)
+o3.io.write_point_cloud('ground_truth.ply',pcd)
+
+pcd = o3.geometry.PointCloud()
+pcd.points = o3.utility.Vector3dVector(points.permute(2, 0, 1).reshape(3, -1).to('cpu').T)
+pcd.colors = o3.utility.Vector3dVector(np.vstack(v_map_colors(pred_choice.reshape(-1).to('cpu'))).T/255)
+o3.io.write_point_cloud('full_predicted.ply', pcd)
---
+++ s3dis_dataset_visualization/s3dis_reducer.py
@@ -0,0 +1,227 @@
+import os
+import re
+from glob import glob
+import time
+import numpy as np
+import pandas as pd
+import open3d as o3
+
+
+
+
+
+
+
+
+ROOT=r'/home/kann/codework/test_pointnet_seg/3D/point_net/Stanford3dDataset_v1.2_Aligned_Version'
+SAVE_PATH = r'/home/kann/codework/test_pointnet_seg/3D/point_net/Stanford3dDataset_v1.2_Reduced_Aligned_Version'
+PARTITION_SAVE_PATH = r'/home/kann/codework/test_pointnet_seg/3D/point_net/Stanford3dDataset_v1.2_Reduced_Partitioned_Aligned_Version'
+
+if not os.path.exists(SAVE_PATH):
+    os.mkdir(SAVE_PATH)
+
+if not os.path.exists(PARTITION_SAVE_PATH):
+    os.mkdir(PARTITION_SAVE_PATH)
+
+CATEGORIES = {
+    'ceiling'  : 0, 
+    'floor'    : 1, 
+    'wall'     : 2, 
+    'beam'     : 3, 
+    'column'   : 4, 
+    'window'   : 5,
+    'door'     : 6, 
+    'table'    : 7, 
+    'chair'    : 8, 
+    'sofa'     : 9, 
+    'bookcase' : 10, 
+    'board'    : 11,
+    'stairs'   : 12,
+    'clutter'  : 13
+}
+
+CATEGORIES = {
+    'ceiling'  : 0,
+    'floor'    : 1,
+    'wall'     : 2,
+    'beam'     : 3,
+    'column'   : 4,
+    'window'   : 5,
+    'door'     : 6,
+    'table'    : 7,
+    'chair'    : 8,
+    'sofa'     : 9,
+    'bookcase' : 10,
+    'board'    : 11,
+    'stairs'   : 12,
+    'clutter'  : 13
+}
+
+area_nums = '1-6' # decide on the number of areas to obtain
+area_dict = {}
+
+# get areas based on split
+areas = glob(os.path.join(ROOT, f'Area_[{area_nums}]*'))
+
+for area in areas:
+    # get all subfolders in area (corresponds to disjoint spaces (or locations))
+    spaces = next(os.walk(area))[1]
+
+    # get dict to store spaces
+    space_dict = {}
+
+    # for each space
+    for space in spaces:
+        space = os.path.join(area, space)
+        annotations = os.path.join(space, 'Annotations')
+
+        # get individual segmentation filepaths
+        segments = glob(os.path.join(annotations, '*.txt'))
+
+        # update space dict
+        space_dict.update({space.split('/')[-1] : segments})
+
+    # update area dict
+    area_dict.update({area.split('/')[-1] : space_dict})
+
+
+def get_space_data(space_segments, categories=CATEGORIES):
+    ''' Obtains space data in (x,y,z),cat format all types are float32
+        Inputs:
+            space_segments - (list) filepaths to all annotaed space segments
+                            for the current space.
+                            e.g. area_dict['Area_1']['conferenceRoom_2']
+            categories - (dict) maps string category to numeric category
+        Outputs:
+            space_data - (array)
+        '''
+    # space data list (x,y,z, cat)
+    space_data = []
+    for seg_path in space_segments:
+
+        # get truth category and xyz points
+        cat = CATEGORIES[seg_path.split('/')[-1].split('_')[0]]
+        xyz = pd.read_csv(seg_path, header=None, sep=' ',
+                          dtype=np.float32, usecols=[0,1,2]).to_numpy()
+
+        # add truth to xyz points and add to space list
+        space_data.append(np.hstack((xyz,
+                                     np.tile(cat, (len(xyz), 1)) \
+                                     .astype(np.float32))))
+
+    # combine into single array and return
+    return np.vstack(space_data)
+
+
+tic = time.time()
+
+for area in area_dict:
+    # create new directory
+    save_dir = os.path.join(SAVE_PATH, area)
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+
+    for space in area_dict[area]:
+        # obtain xyz points with truth labels
+        space_data = pd.DataFrame(get_space_data(area_dict[area][space]))
+
+        # save as .hdf5 file in new directory
+        save_path = os.path.join(save_dir, space + '.hdf5')
+        space_data.to_hdf(save_path, key='space_data')
+
+
+toc = time.time()
+print(toc - tic)
+
+# space_data = pd.read_hdf(os.path.join(save_dir, space + '.hdf5'), key='space_data').to_numpy()
+space_data = get_space_data(area_dict['Area_3']['conferenceRoom_1'], categories=CATEGORIES)
+def get_slice(points, xyz_s, xpart, ypart):
+    ''' Obtains Point Cloud Slices from the (x,y) partitions
+        By default this will obtain roughly 1x1 partitions
+        inputs:
+            points - (array) could be xyz, rgb or any input array
+            xyz_s - (Nx3 array) 0 min shifter point cloud array
+            xpart - xpartitions [[lower, upper]]
+            ypart - ypartitions [[lower, upper]]
+        '''
+    x_slice = (xyz_s[:, 0] >= xpart[0]) \
+              & (xyz_s[:, 0] <= xpart[1])
+
+    y_slice = (xyz_s[:, 1] >= ypart[0]) \
+              & (xyz_s[:, 1] <= ypart[1])
+
+    return points[x_slice & y_slice, :]
+
+
+def get_partitions(xyz, xyz_s, c=1.):
+    ''' Obtains Point Cloud Space Partitions
+        Inputs:
+            xyz_s - (Nx3 array) 0 min shifted point cloud array
+            c - (float) factor for deciding how many partitions to create (larger --> less partitions)
+        Outputs:
+            partitions - (tuple) x and y parition arrays with
+                         format: [[lower, upper]]
+        '''
+    ## get number of x, y bins
+    range_ = np.abs(xyz.max(axis=0) - xyz.min(axis=0))
+    num_xbins, num_ybins, _ = np.uint8(np.round(range_ / c))
+
+    # uncomment this to generate ~1x1m partitions
+    # num_xbins, num_ybins, _ = np.uint8(np.ceil(np.max(xyz_s, 0)))
+
+    ## get x, y bins
+    _, xbins = np.histogram(xyz_s[:, 0], bins=num_xbins)
+    _, ybins = np.histogram(xyz_s[:, 1], bins=num_ybins)
+
+    ## get x y space paritions
+    x_parts = np.vstack((xbins[:-1], xbins[1:])).T
+    y_parts = np.vstack((ybins[:-1], ybins[1:])).T
+
+    return x_parts, y_parts
+
+
+tic = time.time()
+
+num_invalid_partitions = 0
+
+for area in area_dict:
+    # create new directory
+    save_dir = os.path.join(PARTITION_SAVE_PATH, area)
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+
+    for space in area_dict[area]:
+        # obtain xyz points with truth labels
+        space_data = get_space_data(area_dict[area][space])
+
+        # obtain x, y partitions
+        xyz = space_data[:, :3]
+
+        # get 0 min shifted points
+        xyz_s = xyz - xyz.min(axis=0)
+        x_parts, y_parts = get_partitions(xyz, xyz_s, c=1.5)
+
+        # counter for parition saving
+        i = 0
+        for x_part in x_parts:
+            for y_part in y_parts:
+
+                space_slice = pd.DataFrame(get_slice(space_data, xyz_s, x_part, y_part))
+
+                # only save if partition has at least 100 points:
+                if len(space_slice) > 100:
+                    i += 1
+                    save_path = os.path.join(save_dir, space + f'_partition{i}_.hdf5')
+                    space_slice.to_hdf(save_path, key='space_slice')
+                else:
+                    num_invalid_partitions += 1
+
+
+toc = time.time()
+print(toc - tic)
+
+save_path='/home/kann/codework/test_pointnet_seg/3D/point_net/Stanford3dDataset_v1.2_Reduced_Aligned_Version/Area_1/office_20.hdf5'
+space_data = pd.read_hdf(save_path, key='space_slice').to_numpy()
+print(space_data)
+
+
